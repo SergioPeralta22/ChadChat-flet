f872d0ef22b11fa9c268a2edcae7aca14fa4e8c4
Sergio Peralta <seperalta88@gmail.com>
2023-07-06 14:34:17 -0300

    add message class

--- chat.py
+++ chat.py
@@ -1,5 +1,15 @@
 import flet as ft
 
 
+class Message:
+    def __init__(self, username: str, text: str, message_type: str):
+        self.username = username
+        self.text = text
+        self.message_type = message_type
+
+
 def main(page: ft.Page):
     pass
+
+
+ft.app(target=main, assets_dir="assets", view=ft.WEB_BROWSER)
