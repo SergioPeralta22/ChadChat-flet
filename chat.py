def75bdb71f114bb2f03a54631f2ca2dd8ef7a6f
Sergio Peralta <seperalta88@gmail.com>
2023-07-06 21:55:21 -0300

    add basic layout

--- chat.py
+++ chat.py
@@ -14,13 +14,13 @@ class ChatMessage(ft.Row):
         self.vertical_alignment = "start"
         self.controls = [
             ft.CircleAvatar(
-                content=ft.Text(self.get_initials(message.user_name)),
+                content=ft.Text(self.get_initials(message.user)),
                 color=ft.colors.WHITE,
-                bgcolor=self.get_avatar_color(message.user_name),
+                bgcolor=self.get_avatar_color(message.user),
             ),
             ft.Column(
                 [
-                    ft.Text(message.user_name, weight="bold"),
+                    ft.Text(message.user, weight="bold"),
                     ft.Text(message.text, selectable=True),
                 ],
                 tight=True,
@@ -28,10 +28,10 @@ class ChatMessage(ft.Row):
             ),
         ]
 
-    def get_initials(self, user_name: str):
-        return user_name[:1].capitalize()
+    def get_initials(self, user: str):
+        return user[:1].capitalize()
 
-    def get_avatar_color(self, user_name: str):
+    def get_avatar_color(self, user: str):
         colors_lookup = [
             ft.colors.AMBER,
             ft.colors.BLUE,
@@ -47,65 +47,108 @@ class ChatMessage(ft.Row):
             ft.colors.TEAL,
             ft.colors.YELLOW,
         ]
-        return colors_lookup[hash(user_name) % len(colors_lookup)]
+        return colors_lookup[hash(user) % len(colors_lookup)]
 
 
 def main(page: ft.Page):
+    page.horizontal_alignment = "stretch"
     page.title = "ChadGPT-flet"
     chat = ft.Column()
     new_message = ft.TextField()
 
-    def on_message(message: Message):
-        if message.message_type == "chat_message":
-            chat.controls.append(ft.Text(f"{message.user}: {message.text}"))
-        elif message.message_type == "login_message":
-            chat.controls.append(
-                ft.Text(message.text, italic=True, color=ft.colors.BLACK45, size=12)
-            )
-        page.update()
-
-    page.pubsub.subscribe(on_message)  # type: ignore
-
-    def send_click(e):
-        page.pubsub.send_all(
-            Message(
-                user=page.session.get("user_name"),
-                text=new_message.value,
-                message_type="chat_message",
-            )
-        )
-        new_message.value = ""
-        page.update()
-
-    def join_click(e):
-        if not user_name.value:
-            user_name.error_text = "Name cannot be blank!"
-            user_name.update()
+    def join_chat_click(e):
+        if not join_user_name.value:
+            join_user_name.error_text = "Name cannot be blank!"
+            join_user_name.update()
         else:
-            page.session.set("user_name", user_name.value)
+            page.session.set("user", join_user_name.value)
             page.dialog.open = False
+            new_message.prefix = ft.Text(f"{join_user_name.value}: ")
             page.pubsub.send_all(
                 Message(
-                    user=user_name.value,
-                    text=f"{user_name.value} has joined the chat.",
+                    user=join_user_name.value,
+                    text=f"{join_user_name.value} has joined the chat.",
                     message_type="login_message",
                 )
             )
             page.update()
 
-    user_name = ft.TextField(label="Enter your name")
+    def send_message_click(e):
+        if new_message.value != "":
+            page.pubsub.send_all(
+                Message(
+                    page.session.get("user"),
+                    new_message.value,
+                    message_type="chat_message",
+                )
+            )
+            new_message.value = ""
+            new_message.focus()
+            page.update()
+
+    def on_message(message: Message):
+        if message.message_type == "chat_message":
+            m = ChatMessage(message)
+        elif message.message_type == "login_message":
+            m = ft.Text(message.text, italic=True, color=ft.colors.BLACK45, size=12)
+        chat.controls.append(m)
+        page.update()
+
+    page.pubsub.subscribe(on_message)  # type: ignore
+
+    join_user_name = ft.TextField(
+        label="Enter your name to join the chat",
+        autofocus=True,
+        on_submit=join_chat_click,
+    )
 
     page.dialog = ft.AlertDialog(
         open=True,
         modal=True,
         title=ft.Text("Welcome!"),
-        content=ft.Column([user_name], tight=True),
-        actions=[ft.ElevatedButton(text="Join chat", on_click=join_click)],  # type: ignore
-        actions_alignment="end",  # type: ignore
+        content=ft.Column([join_user_name], width=300, height=70, tight=True),
+        actions=[ft.ElevatedButton(text="Join chat", on_click=join_chat_click)],
+        actions_alignment="end",
     )
 
+    # Chat messages
+    chat = ft.ListView(
+        expand=True,
+        spacing=10,
+        auto_scroll=True,
+    )
+
+    # A new message entry form
+    new_message = ft.TextField(
+        hint_text="Write a message...",
+        autofocus=True,
+        shift_enter=True,
+        min_lines=1,
+        max_lines=5,
+        filled=True,
+        expand=True,
+        on_submit=send_message_click,
+    )
+
+    # Add everything to the page
     page.add(
-        chat, ft.Row([new_message, ft.ElevatedButton("Send", on_click=send_click)])  # type: ignore
+        ft.Container(
+            content=chat,
+            border=ft.border.all(1, ft.colors.OUTLINE),
+            border_radius=5,
+            padding=10,
+            expand=True,
+        ),
+        ft.Row(
+            [
+                new_message,
+                ft.IconButton(
+                    icon=ft.icons.SEND_ROUNDED,
+                    tooltip="Send message",
+                    on_click=send_message_click,
+                ),
+            ]
+        ),
     )
 
 
